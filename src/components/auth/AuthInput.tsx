53342446371c6c7dde019d04e7018a72cbb719a7
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-01 15:49:36 -0300

    feat: screem login styled

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -7,18 +7,40 @@ interface AuthInputProps {
   onChange: (value: any) => void
 }
 
-export default function AuthInput(props: AuthInputProps) {
-  return props.isRendered ? null : (
-    <div className="flex flex-col">
-      <label htmlFor="">
-        {props.label}
-      </label>
+/**
+ * Renders an authentication input component.
+ *
+ * @param {AuthInputProps} props - The props object containing the input component properties.
+ * @return {JSX.Element | null} The rendered authentication input component.
+ */
+export default function AuthInput(props: AuthInputProps): JSX.Element | null {
+  if (props.isRendered) {
+    return null;
+  }
+
+  /**
+   * Handles the change event of an input element.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} e - The change event object.
+   * @return {void} Returns nothing.
+   */
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    props.onChange?.(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col mt-4">
+      <label htmlFor="">{props.label}</label>
       <input
         type={props.type ?? 'text'}
         value={props.value}
         required={props.required}
-        onChange={e => props.onChange?.(e.target.value)}
+        onChange={handleChange}
+        className={`
+          px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+          focus:bg-white focus:outline-none transition duration-500
+        `}
       />
     </div>
-  )
+  );
 }
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import AuthInput from "@/components/auth/AuthInput";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function auth() {
@@ -7,25 +7,95 @@ export default function auth() {
   const [password, setPassword] = useState<string>('')
   const [mode, setMode] = useState<'login' | 'register'>('login')
 
+  const handleSubmit = () => {
+    if (mode === 'login') {
+      console.log('login')
+    } else {
+      console.log('register')
+    }
+  }
+
   return (
-    <div>
-      <h1>
-        Login
-      </h1>
-      <AuthInput
-        label="Email"
-        type="email"
-        value={email}
-        required
-        onChange={(e: string) => setEmail(e)}
-      />
-      <AuthInput
-        label="Senha"
-        type="password"
-        value={password}
-        required
-        onChange={(e: string) => setPassword(e)}
-      />
+    <div className="flex h-screen items-center justify-center">
+      <div className="hidden md:block md:w-1/2 lg:w-2/3">
+        <img
+          src="https://source.unsplash.com/random"
+          alt="Imagem da tela de autenticação"
+          className="h-screen w-full object-cover bg-no-repeat"
+        />
+      </div>
+      <div className="w-full md:w-[600px] lg:h-1/3 m-10">
+        <h1 className={`
+        text-xl font-bold mb-5
+      `}>
+          {mode === 'login' ? 'Entre com a sua conta' : 'Cadastre-se na plataforma'}
+        </h1>
+        <AuthInput
+          label="Email"
+          type="email"
+          value={email}
+          required
+          onChange={(e: string) => setEmail(e)}
+        />
+        <AuthInput
+          label="Senha"
+          type="password"
+          value={password}
+          required
+          onChange={(e: string) => setPassword(e)}
+        />
+        <button
+          onClick={handleSubmit}
+          className={`
+          w-full bg-indigo-500 hover:bg-indigo-400 text-white
+          rounded-lg px-4 py-3 mt-6 font-semibold
+        `}
+        >
+          {mode === 'login' ? 'Entrar' : 'Cadastrar'}
+        </button>
+        <hr className="my-6 border-gray-300 w-full" />
+        <button
+          onClick={() => setMode(mode === 'login' ? 'register' : 'login')}
+          className={`
+          w-full bg-transparent border-2 border-red-500 hover:border-red-400
+          text-gray-400 rounded-lg px-4 py-3 font-semibold flex justify-center
+        `}
+        >
+          <Image
+            src="/google.svg"
+            alt="Logo do Google"
+            width={24}
+            height={24}
+            className="mr-2"
+          />
+          Entrar com conta google
+        </button>
+        {mode == 'login' ? (
+          <p className="mt-8">
+            Novo por aqui?
+            <a
+              onClick={() => setMode('register')}
+              className={`
+              text-blue-500 hover:text-blue-700 font-semibold cursor-pointer ml-2
+            `}
+            >
+              Crie uma conta gratuitamente
+            </a>
+          </p>
+        ) : (
+          <p className="mt-8">
+            Já faz parte da nossa comunidade?
+            <a
+              onClick={() => setMode('login')}
+              className={`
+              text-blue-500 hover:text-blue-700 font-semibold cursor-pointer ml-2
+            `}
+            >
+              Entre com suas credenciais
+            </a>
+          </p>
+        )}
+      </div>
     </div>
   )
 }
