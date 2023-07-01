c28ddef2cff5c6100591b5e2bddbddac8a50f343
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-30 22:43:47 -0300

    feat: enable darkMode class in tailwindcss'

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -20,7 +20,7 @@ export default function Layout({ title, subtitle, children }: LayoutProps): JSX.
       flex w-screen h-screen
     `}>
       <MenuAside />
-      <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+      <div className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-900`}>
         <Header title={title} subtitle={subtitle} />
         <Content>
           {children}
