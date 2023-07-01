4eff1932558384a7abad9eabcef6270dedf19289
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-01 08:38:21 -0300

    feat: styled title e subtitle

--- src/components/template/Title.tsx
+++ src/components/template/Title.tsx
@@ -13,10 +13,10 @@ export default function Title({ title, subtitle }: TitleProps): JSX.Element {
   return (
     <div>
       <h1 className={`
-
+        font-black text-4xl text-gray-900
       `}>{title}</h1>
       <h2 className={`
-        
+        font-light text-base text-gray-600
       `}>{subtitle}</h2>
     </div>
   )
