1d5a046a1527cfa259daa36469e954036ee0f1f0
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-01 08:35:01 -0300

    feat: create link menu aside

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface MenuItemProps {
   url: string
   text: string
@@ -14,8 +16,17 @@ interface MenuItemProps {
  */
 export default function MenuItem({ url, text, icon }: MenuItemProps): JSX.Element {
   return (
-    <li>
-      {icon}
+    <li className={`
+      hover:bg-gray-100
+    `}>
+      <Link href={url} className={`
+        flex flex-col justify-center items-center h-20 w-20
+      `}>
+        {icon}
+        <span className={`
+          text-sm font-light text-gray-600 mt-1
+        `}>{text}</span>
+      </Link>
     </li>
   )
 }
---
+++ src/pages/profile.tsx
@@ -0,0 +1,15 @@
+import Layout from '@/components/template/Layout'
+import React from 'react'
+
+/**
+ * Renders the index component.
+ *
+ * @return {JSX.Element} The rendered index component.
+ */
+export default function profile(): JSX.Element {
+  return (
+    <Layout title="Profile" subtitle="Profile">
+      <h3>Profile</h3>
+    </Layout>
+  )
+}
